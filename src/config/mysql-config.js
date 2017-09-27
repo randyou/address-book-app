1fa8843eefa3fce774719d86f4eaba9462ea0833
Randy Ou <ourd2012@gmail.com>
2017-09-27 19:11:10 +0800

    Fix mysql url env var.

--- src/config/mysql-config.js
+++ src/config/mysql-config.js
@@ -6,7 +6,7 @@ const config = {
   password: process.env.MYSQL_P || 'www',
   host: process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || 3306,
-  uri:process.env.CLEARDB_DATABASE_UR || 'mysql://www:www@localhost/addressbook?reconnect=true'
+  uri: process.env.CLEARDB_DATABASE_URL || 'mysql://www:www@localhost/addressbook?reconnect=true'
 };
 
 export default config
